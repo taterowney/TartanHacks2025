3e5fd8990990fb1cb49de7ea5e17ecf7e217fdc8
taterowney <51540870+taterowney@users.noreply.github.com>
2025-02-08 16:51:16 -0500

    Lights + CV

--- lights.py
+++ lights.py
@@ -9,9 +9,9 @@ address = 0x13
 bus = smbus.SMBus(1)
 
 p = neopixel.NeoPixel(board.D18, 50)
-p.fill((128, 128, 128))
+p.fill((64, 64, 64))
 while True:
     if bus.read_byte(address) >= 0x20:
         p.fill((0,0,0))
-    else: p.fill((128, 128, 128))
+    else: p.fill((64, 64, 64))
     time.sleep(0.5)
